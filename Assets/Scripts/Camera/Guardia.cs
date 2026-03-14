7cc88e0b586809a42b2ad4439e2fc50ab8220b5e
DoubleMii <sebas.diaz.lugo@gmail.com>
2026-03-14 21:49:57 +0100

    gg
    gg

--- Assets/Scripts/Camera/Guardia.cs
+++ Assets/Scripts/Camera/Guardia.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
 
-public class Guardia : MonoBehaviour
+public class Guardian : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform player;
+    [SerializeField] Transform pivotPOV;
+    [SerializeField] float detectionRange = 10f;
+    [SerializeField] float rotationSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 dirToPlayer = player.position - pivotPOV.position;
+
+        // Magnitud manual
+        float distSqr = dirToPlayer.x * dirToPlayer.x
+                      + dirToPlayer.y * dirToPlayer.y
+                      + dirToPlayer.z * dirToPlayer.z;
+
+        if (distSqr < detectionRange * detectionRange)
+        {
+            float dist = Mathf.Sqrt(distSqr);
+            Vector3 dirUnit = new Vector3(dirToPlayer.x / dist,
+                                          dirToPlayer.y / dist,
+                                          dirToPlayer.z / dist);
+
+            float dot = Vector3.Dot(pivotPOV.forward, dirUnit);
+            Debug.Log("Dot product: " + dot);
+
+            // Quaternion que representa "mirar hacia dirUnit"
+            // FromToRotation: rotación desde el forward actual hasta la dirección objetivo
+            Quaternion deltaRot = Quaternion.FromToRotation(pivotPOV.forward, dirUnit);
+
+            // Aplicar suavemente cada frame
+            pivotPOV.rotation = Quaternion.Slerp(
+                pivotPOV.rotation,
+                pivotPOV.rotation * deltaRot,
+                rotationSpeed * Time.deltaTime
+            );
+        }
     }
 }
